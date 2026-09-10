6450e2aa986ffdeb534abc68fad4d651b95e1886
dogtective <raphael.signori@gmail.com>
2026-09-10 15:36:42 +0200

    11/09/26 - 1

--- frontend/src/components/Avatar/Avatar.test.jsx
+++ frontend/src/components/Avatar/Avatar.test.jsx
@@ -4,14 +4,23 @@ import Avatar from "./Avatar";
 
 describe("Exo Avatar", () => {
     test("B4 - Avatar", () => {
-        render(<Avatar user={{ full_name: "Camille Client" }}/>);
+        render(<Avatar user={{ full_name: "Camille Client" }} />);
         const avatar = screen.getByText('CC');
         expect(avatar).toBeInTheDocument();
     });
 
     test("B5 - Avatar", () => {
-        render(<Avatar user={{ email: "client@eventflow.test"}} />);
+        render(<Avatar user={{ email: "client@eventflow.test" }} />);
         const avatar = screen.getByText('C');
         expect(avatar).toBeInTheDocument();
+    });
+});
+
+
+describe("Exo Avatar 2", () => {
+    test("C3 - Avatar", () => {
+        render(<Avatar size={50} />);
+        const avatar = screen.getByText("?");
+        expect(avatar).toHaveStyle("width: 50px ; height: 50px");
     });
 });
--- frontend/src/components/Logo/Logo.test.jsx
+++ frontend/src/components/Logo/Logo.test.jsx
@@ -2,9 +2,11 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from 'vitest';
 import Logo from "./Logo";
 
+
 test('Intro test', () => {
     expect(true).toBe(true);
 });
+
 
 describe('Exo Logo', () => {
     test('B1 - Logo', () => {
@@ -28,3 +30,17 @@ describe('Exo Logo', () => {
     });
 });
 
+
+describe("Exo Logo 2", () => {
+    test("C1 - Logo", () => {
+        render(<Logo size={60} />);
+        const logo = screen.getByLabelText('eventflow');
+        expect(logo).toHaveAttribute("width" , "60");
+    });
+
+    test('C2 - Logo', () => {
+        render(<Logo size={18} />);
+        const logo = screen.getByLabelText('eventflow');
+        expect(logo).toHaveAttribute("width", "18");
+    });
+});
